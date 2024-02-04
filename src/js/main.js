282461aab00e2fc2f919a15ef3009698c5d2273d
Brenda-A-S <brenda.antunes16@gmail.com>
2024-02-04 16:19:52 -0300

    style: update banner & header style

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import initFaqs from './modules/faq.js';
 const scroll = new AnimacaoScroll('[data-anime="scroll"]');
 scroll.init();
 
-const toggleMenu = new ToggleMenu('.header-container', '.menu-button', '.menu-list', 'active');
+const toggleMenu = new ToggleMenu('.header-bg', '.menu-button', '.menu-list', 'active');
 toggleMenu.init();
 
 initFaqs();
