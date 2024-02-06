52c97928494a8bf9571ba1bed157eed9620f7471
Brenda-A-S <brenda.antunes16@gmail.com>
2024-02-05 22:50:05 -0300

    terminei por enquanto

--- src/js/modules/faq.js
+++ src/js/modules/faq.js
@@ -5,6 +5,7 @@ export default function initFaqs() {
     const controls = element.getAttribute('aria-controls');
     const id = document.getElementById(controls);
     id.classList.toggle('active');
+    console.log()
     const active = id.classList.contains('active');
     element.setAttribute('aria-expanded', active);
   }
@@ -13,7 +14,7 @@ export default function initFaqs() {
     element.addEventListener('click', toggleClass);
   }
 
-  const questions = document.querySelectorAll('.perguntas button');
+  const questions = document.querySelectorAll('.faq button');
 
   questions.forEach(toggleActive);
 }
